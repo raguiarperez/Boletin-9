bcc036a53fb3c5c07d584c5d42027209b0c3abbc
raguiarperez <raguiarperez@danielcastelao.org>
2018-11-13 11:50:36 +0100

    Boletin 9.6

---
+++ Boletin9_6/src/boletin9_6/Boletin9_6.java
@@ -0,0 +1,14 @@
+package boletin9_6;
+
+/**
+ *
+ * @author rafa
+ */
+public class Boletin9_6 {
+
+    public static void main(String[] args) {
+        soldos obx = new soldos();
+        obx.lecturaSueldos();
+    }
+    
+}
---
+++ Boletin9_6/src/boletin9_6/soldos.java
@@ -0,0 +1,33 @@
+
+package boletin9_6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rafa
+ */
+public class soldos {
+        public void lecturaSueldos(){
+        double soldo;
+        int contTrabajadores = 0;
+        int contSueldos1 = 0;
+        int contSueldos2 = 0;
+        do{
+            soldo = Double.parseDouble(JOptionPane.showInputDialog("sueldo de un empleado:"));
+            if(soldo > 0){
+                contTrabajadores++;
+            }
+            if(soldo >= 1000 && soldo <= 1750){
+                contSueldos1++;
+            }
+            else if(soldo < 1000 && soldo > 0){
+                contSueldos2++;
+            }
+        }while(soldo != 0);
+        JOptionPane.showMessageDialog(null, "Número de trabajadores que cobran entre 1000 y 1750€: " 
+                + contSueldos1 + "\nPorcentaje de trabajadores que cobran menos de 1000€: " 
+                + (contSueldos2*100/contTrabajadores));
+    }
+}
+
