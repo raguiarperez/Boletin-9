a26f168ed6e653fa94b2903318442337f640d831
raguiarperez <raguiarperez@danielcastelao.org>
2018-11-13 11:33:53 +0100

    Boletin 9.5

---
+++ Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,14 @@
+package boletin9_5;
+
+/**
+ *
+ * @author rafa
+ */
+public class Boletin9_5 {
+
+    public static void main(String[] args) {
+        elementos serie = new elementos();
+        serie.serieComas();
+    }
+
+}
